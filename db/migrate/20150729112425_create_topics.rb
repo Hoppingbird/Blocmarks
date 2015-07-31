e9991e9c78156f9bbe458b2365f5e540a8e72a2d
Hoppingbird <hoppingbird@gmail.com>
2015-07-30 21:47:52 -0400

    topis fix

--- db/migrate/20150729112425_create_topics.rb
+++ db/migrate/20150729112425_create_topics.rb
@@ -2,9 +2,10 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :title
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :topics, :users
   end
 end
