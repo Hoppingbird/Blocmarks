0b80463dab37835513600c18a32c578332fd1207
Hoppingbird <hoppingbird@gmail.com>
2015-07-30 22:36:00 -0400

    foreign keys

--- db/migrate/20150729112642_create_bookmarks.rb
+++ db/migrate/20150729112642_create_bookmarks.rb
@@ -2,9 +2,10 @@ class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
       t.string :url
-      t.references :topic, index: true, foreign_key: true
+      t.references :topic, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :bookmarks, :topics
   end
 end
